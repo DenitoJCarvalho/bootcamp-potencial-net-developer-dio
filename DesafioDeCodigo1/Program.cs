a23949c1ba6b463376f8b4a9e71b87d3b6c68b80
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-10-10 15:14:45 -0300

    resolvido desafio numero 1

--- DesafioDeCodigo1/Program.cs
+++ DesafioDeCodigo1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Dado um número inteiro (n), retorne a diferença entre o produto de seus digitos e a soma de seus digitos
+using System;
+
+class Dio
+{
+  static void Main(string[] args)
+  {
+    try
+    {
+      int n = int.Parse(Console.ReadLine());
+      int p = 1, s = 0;
+
+      while (n > 0)
+      {
+        int l = n % 10;
+
+        p *= l;
+        s += l;
+        n /= 10;
+      }
+
+      Console.WriteLine(p - s);
+
+    }
+    catch (Exception error)
+    {
+      Console.WriteLine($"Erro: {error.Message}");
+    }
+  }
+
+}
