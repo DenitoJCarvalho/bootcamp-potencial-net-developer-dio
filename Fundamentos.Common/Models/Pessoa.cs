e0d6d993244eb1cc52147b0490c2c3d2f9b09534
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-10-10 13:34:21 -0300

    inserido alguns comentarios pra documentacao

--- Fundamentos.Common/Models/Pessoa.cs
+++ Fundamentos.Common/Models/Pessoa.cs
@@ -7,14 +7,19 @@ namespace Fundamentos.Common.Models
 {
   //=> começo do escopo
   //=> Nas classes sempre usar PascalCase e para  uma boa leitura usar o mesmo nome do arquivo
+  /// <summary>
+  /// Representa uma pessoa física
+  /// </summary>
   public class Pessoa
   {
     public string? Nome { get; set; } //=> propriedade, get = pegavalor, set = atribui valor
     public int Idade { get; set; } //=> propriedade, get = pegavalor, set = atribui valor
     public string? Sobrenome { get; set; } //=> nas propriedades sempre usar PascalCase
     public DateTime DataDeNascimento { get; set; }
-
-    public void Apresentar() //=> método = ação,
+    /// <summary>
+    ///   Exibe uma apresentação sa pessoa informando nome, idade e data de nascimento
+    /// </summary>
+    public void Apresentar()
     {
       Console.WriteLine(@$"
         Olá, meu nome é {Nome} {Sobrenome.ToUpper()}, 
