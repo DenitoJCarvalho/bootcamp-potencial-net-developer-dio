833a683ed1df46b8b2c599974027a386cdbd481d
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-10-15 18:53:42 -0300

    concluido o desafio 3

--- DesafioDeCodigo3/Program.cs
+++ DesafioDeCodigo3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Desafio
+Dado um inteiro n, retorne true se n tiver exatamente três divisores positivos. Caso contrário, retorne false. O inteiro m é um divisor de n, se existe um inteiro k tal que n = k * m. 
+
+Entrada
+O arquivo de entrada consiste em um inteiro n que será validada caso esteja sob as condições do desafio. */
+
+///<sumary>
+/// A saída consistirá em um tipo booleano: true o false
+///</sumary>
+
+using System;
+
+class Solution
+{
+  public static void Main(string[] args)
+  {
+    int n = int.Parse(Console.ReadLine());
+    int count = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+      if (n % i == 0)
+      {
+        count++;
+      }
+
+      if (count > 3)
+      {
+        Console.WriteLine(false);
+      }
+    }
+
+    Console.WriteLine(count == 3);
+  }
+}
