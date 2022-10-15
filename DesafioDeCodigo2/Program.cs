9c4a55f605e3bc9303af488d25df13c8c726558d
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-10-15 17:43:08 -0300

    resolvido desafio 2

--- DesafioDeCodigo2/Program.cs
+++ DesafioDeCodigo2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Linq;
+
+class Dio
+{
+  static void Main(string[] args)
+  {
+    int qt = int.Parse(Console.ReadLine());
+
+    for (var i = 0; i < qt; i++)
+    {
+
+      string[] v = Console.ReadLine().Split(" ");
+      string a = v[0];
+      string b = v[1];
+
+      if (!a.EndsWith(b))
+      {
+        Console.WriteLine("não encaixa");
+      }
+      else if (a.EndsWith(b))
+      {
+        Console.WriteLine("encaixa");
+      }
+      else
+      {
+        Console.WriteLine("não encaixa");
+      }
+    }
+  }
+}
