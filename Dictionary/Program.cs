608ce5f2dce79d4705fe335eaeac3e4ec4abbb91
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-10-25 14:36:44 -0300

    Finalizado estudo sobre dicionarios

---
+++ Dictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("RS", "Rio Grande do Sul");
+estados.Add("SC", "Santa Catarina");
+estados.Add("MG", "Minas Gerais");
+estados.Add("BA", "Bahia");
+
+foreach (var item in estados)
+{
+  Console.WriteLine($"Chave: {item.Key} - Valor: {item.Value}");
+}
+
+
+Console.WriteLine($"Chave removida - {estados.Remove("BA")}");
