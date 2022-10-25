a1c8813af9b40725ff01bf517907cbeec4594e10
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-10-25 20:12:25 -0300

    Finalizando estudo sobre descontrutores

--- Desconstrutor/Program.cs
+++ Desconstrutor/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Desconstrutor.Models;
+
+
+Pessoa p1 = new Pessoa("Adrian", 2);
+
+(string nome, int idade) = p1;
+
+Console.WriteLine($"{nome} - {idade}");
