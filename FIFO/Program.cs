2da3c226d880cfc64cde9b0043eb1f90bbaa7bc0
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-10-24 21:09:49 -0300

    Finalizando estudos sobre filas, utilizando o metodo enqueue() e dequeue()

--- FIFO/Program.cs
+++ FIFO/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Queue<int> fila = new Queue<int>();
+
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(5);
+fila.Enqueue(8);
+fila.Enqueue(11);
+
+foreach (int item in fila)
+{
+  Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+
+
+foreach (int item in fila)
+{
+  Console.WriteLine(item);
+}
