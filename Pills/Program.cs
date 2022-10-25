bb35e1561f9a0634592141bdefcf0e28ca3abeea
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-10-24 21:21:24 -0300

    Finalizando estudos sobre pilhas - stacks. Usando os metodos stack.push() e stack.pop()

--- Pills/Program.cs
+++ Pills/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Stack<int> pilha = new Stack<int>();
+
+pilha.Push(2);
+pilha.Push(4);
+pilha.Push(5);
+pilha.Push(6);
+pilha.Push(7);
+pilha.Push(12);
+
+foreach (int item in pilha)
+{
+  Console.WriteLine(item);
+}
+
+pilha.Pop();
+pilha.Push(100);
+
+Console.WriteLine($"Removendo o elemento do topo {pilha.Pop()}");
+
+foreach (int item in pilha)
+{
+  Console.WriteLine(item);
+}
+
