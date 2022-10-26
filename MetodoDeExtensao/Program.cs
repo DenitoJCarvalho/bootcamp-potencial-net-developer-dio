83460e66385388d16373e72ce44ddc78e0535328
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-10-26 15:11:30 -0300

    Finalizando estudos sobre metodos extensions

--- MetodoDeExtensao/Program.cs
+++ MetodoDeExtensao/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MetodoDeExtensao.Models;
+
+int numero = Convert.ToInt32(Console.ReadLine());
+bool par = false;
+
+par = numero.isPar();
+
+string msg = $"O número {numero} é {(par ? "par" : "ímpar")}";
+
+Console.WriteLine(msg);
+
