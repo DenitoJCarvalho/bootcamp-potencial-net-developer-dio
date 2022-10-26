8e53f174764f7c96b0bc8209561d0c3e8c7e05f7
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-10-26 14:57:55 -0300

    Finalizando estudo sobre classes genericas

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Generics.Models;
+
+Generico<int> arrayInt = new Generico<int>();
+
+arrayInt.AdicionarelementoArray(12);
+
+Console.WriteLine(arrayInt[0]);
