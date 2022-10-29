5c3328ff4f80dc6ea0a763c3344e020195417e70
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-10-29 19:55:34 -0300

    Iniciando desafio 4

---
+++ DesafioDeCodigo4/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Dado um array nums de tamanho n, retorne o elemento majoritário, isto é, o elemento que aparece o maior número de vezes no seu array.
+*/
+
+using System;
+using System.Text.RegularExpressions;
+
+int n = int.Parse(Console.ReadLine());
+
+int[] num = new int[n];
+
+for (var i = 0; i < num.Length; i++)
+{
+  num[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine(MajorityElement(num));
+
+static int MajorityElement(int[] nums)
+{
+  int major = nums[0];
+  int count = 1;
+
+  for (var i = 0; i < nums.Length; i++)
+  {
+    if (major == nums[count++])
+    {
+      major = nums[i];
+      count++;
+    }
+    else
+    {
+      if (major == nums[i])
+      {
+        count++;
+      }
+      else
+      {
+        count--;
+      }
+    }
+  }
+
+  return major;
+
+}
