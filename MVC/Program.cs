8dac890aa592bd990c56c3eb9ad85b2be16bde1e
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2022-11-12 21:48:06 -0300

    configurando ef com banco de dados

--- MVC/Program.cs
+++ MVC/Program.cs
@@ -1,4 +1,14 @@
+using MVC.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//conexão com o banco de dados
+var connString = builder.Configuration.GetConnectionString("ConexaoMySQL");
+var serverVersion = new MySqlServerVersion("8.0");
+
+builder.Services.AddDbContext(options => options.UseMySql(connString, serverVersion));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -8,9 +18,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
